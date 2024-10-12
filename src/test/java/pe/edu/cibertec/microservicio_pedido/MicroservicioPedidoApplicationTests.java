495fd5eacc8aee984d376588c5612086e15a88f4
Natha23XO <hugocastroleon2301@gmail.com>
2024-10-11 21:00:14 -0500

    subiendo

---
+++ src/test/java/pe/edu/cibertec/microservicio_pedido/MicroservicioPedidoApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.microservicio_pedido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioPedidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
